1232d0e5b6a5d1d3ddc41f30a7b3acde06e1cbdc
Neela Krishna Teja Tadikonda <ntadiko@ncsu.edu>
2016-10-07 02:17:43 -0400

    .

--- app/entities/movie/movie.state.js
+++ app/entities/movie/movie.state.js
@@ -13,7 +13,7 @@
                 parent: 'entity',
                 url: '/movie',
                 data: {
-                    authorities: ['ROLE_USER'],
+                    authorities: [],
                     pageTitle: 'Movies'
                 },
                 views: {
